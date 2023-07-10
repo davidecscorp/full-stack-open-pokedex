08646b05666e9f125244992d3ace17351338c12a
David Bodunrin <bodunrind@ecscorpresources.com>
2023-07-10 15:34:23 +0100

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   "env": {
-    // "browser": true,
+    "browser": true,
     "node": true,
     "es6": true,
     "jest/globals": true
@@ -46,5 +46,10 @@ module.exports = {
     ],
     // "no-console": "error",
     "react/prop-types": 0
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
   }
 }
